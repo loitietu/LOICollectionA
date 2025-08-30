05778d36d46a23a5c120c8be05001a9a8ac53fc9
loitietu <1953442980@qq.com>
2025-08-30 23:46:40 +0800

    refactor: optimized the tr function of i18n

--- src/plugin/utils/I18nUtils.cpp
+++ src/plugin/utils/I18nUtils.cpp
@@ -29,17 +29,16 @@ void I18nUtils::set(const std::string& local, const std::string& key, const std:
     this->data[local].insert({key, value});
 }
 
-std::string I18nUtils::get(const std::string& local, const std::string& key) {
+bool I18nUtils::has(const std::string& local) const {
+    return this->data.find(local) != this->data.end();
+}
+
+std::string I18nUtils::get(const std::string& local, const std::string& key) const {
     if (auto localIt = this->data.find(local); localIt != data.end()) {
         const std::unordered_map<std::string, std::string>& translations = localIt->second;
         if (auto keyIt = translations.find(key); keyIt != translations.end())
             return keyIt->second;
     }
-        
-    if (auto defaultIt = this->data.find(this->defaultLocale); defaultIt != data.end()) {
-        const std::unordered_map<std::string, std::string>& translations = defaultIt->second;
-        if (auto keyIt = translations.find(key); keyIt != translations.end())
-            return keyIt->second;
-    }
+    
     return key;
 }
--- src/plugin/utils/I18nUtils.h
+++ src/plugin/utils/I18nUtils.h
@@ -16,7 +16,9 @@ public:
 
     void set(const std::string& local, const std::string& key, const std::string& value);
 
-    std::string get(const std::string& local, const std::string& key);
+    [[nodiscard]] bool has(const std::string& local) const;
+
+    [[nodiscard]] std::string get(const std::string& local, const std::string& key) const;
 public:
     std::unordered_map<std::string, std::unordered_map<std::string, std::string>> data;
     std::string defaultLocale{};
@@ -24,6 +26,8 @@ public:
 
 namespace I18nUtilsTools {
     inline std::string tr(const std::string& local, const std::string& key) {
-        return I18nUtils::getInstance()->get(local, key);
+        return I18nUtils::getInstance()->get(
+            I18nUtils::getInstance()->has(local) ? local : I18nUtils::getInstance()->defaultLocale, key
+        );
     }
 }
